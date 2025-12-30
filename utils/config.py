1f29fba115bc68bce257d17f6ef07819567a3f11
BtwToxic <btwalexxd@gmail.com>
2025-12-30 20:59:02 +0530

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -22,7 +22,7 @@ apiflash_key = "76b05dd14bf148f49e1dadc98b5f61cf"
 rmbg_key = os.getenv("RMBG_KEY", env.str("RMBG_KEY", ""))
 vt_key = "26809ec198954a226106354659c8da99c68148c6883f60f2c457b33cfe3b217d"
 gemini_key = os.getenv("GEMINI_KEY", env.str("GEMINI_KEY", ""))
-cohere_key = os.getenv("COHERE_KEY", env.str("COHERE_KEY", ""))
+cohere_key = os.getenv("KEY")
 
 pm_limit = int(os.getenv("PM_LIMIT", env.int("PM_LIMIT", 4)))
 
