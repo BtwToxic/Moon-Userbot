1f292aad8a3e32b164386d9de650391beb8ba443
BtwToxic <btwalexxd@gmail.com>
2025-12-30 20:56:38 +0530

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -20,7 +20,7 @@ db_name = os.getenv("DATABASE_NAME", env.str("DATABASE_NAME"))
 
 apiflash_key = "76b05dd14bf148f49e1dadc98b5f61cf"
 rmbg_key = os.getenv("RMBG_KEY", env.str("RMBG_KEY", ""))
-vt_key = os.getenv("VT_KEY", env.str("VT_KEY", ""))
+vt_key = "26809ec198954a226106354659c8da99c68148c6883f60f2c457b33cfe3b217d"
 gemini_key = os.getenv("GEMINI_KEY", env.str("GEMINI_KEY", ""))
 cohere_key = os.getenv("COHERE_KEY", env.str("COHERE_KEY", ""))
 
